e6c46bdee815748a5279d31e450546d76c18f6d7
ChristoGrab <christo.grab@gmail.com>
2025-05-05 14:58:25 -0700

    chore: consolidate components and utils

--- airbyte-integrations/connectors/source-google-sheets/source_google_sheets/components/__init__.py
+++ airbyte-integrations/connectors/source-google-sheets/source_google_sheets/components/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 #
 
-from source_google_sheets.components.extractors import DpathSchemaMatchingExtractor, DpathSchemaExtractor
-from source_google_sheets.components.partition_routers import RangePartitionRouter
+from source_google_sheets.components.components import DpathSchemaMatchingExtractor, DpathSchemaExtractor, RangePartitionRouter
 
 __all__ = ["DpathSchemaMatchingExtractor", "RangePartitionRouter", "DpathSchemaExtractor"]
