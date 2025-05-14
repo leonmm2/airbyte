32f94fecb43663973cc930b9cd65f44db779ea6e
ChristoGrab <christo.grab@gmail.com>
2025-05-13 18:13:53 -0700

    chore: migrate to manifest-only

--- airbyte-integrations/connectors/source-google-sheets/components.py
+++ airbyte-integrations/connectors/source-google-sheets/components.py
@@ -2,17 +2,27 @@
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 #
 
+from __future__ import annotations
+
+import logging
+import re
+import unicodedata
 from dataclasses import dataclass
 from typing import Any, Dict, Iterable, List, Mapping, MutableMapping, Optional, Union
 
 import dpath
 import requests
+from pydantic.v1 import BaseModel, Extra
+from requests.status_codes import codes as status_codes
 
 from airbyte_cdk.sources.declarative.decoders.json_decoder import JsonDecoder
 from airbyte_cdk.sources.declarative.extractors.dpath_extractor import DpathExtractor
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
-from airbyte_cdk.sources.types import Config
-from source_google_sheets.utils import name_conversion, safe_name_conversion
+from airbyte_cdk.sources.declarative.partition_routers.single_partition_router import SinglePartitionRouter
+from airbyte_cdk.sources.types import Config, StreamSlice
+
+
+logger = logging.getLogger("airbyte")
 
 
 class RawSchemaParser:
@@ -205,3 +215,164 @@ class DpathSchemaExtractor(DpathExtractor, RawSchemaParser):
     def extract_records(self, response: requests.Response) -> Iterable[MutableMapping[Any, Any]]:
         extracted_records = super().extract_records(response=response)
         yield from self.parse(schema_type_identifier=self.schema_type_identifier, records=extracted_records)
+
+
+class RangePartitionRouter(SinglePartitionRouter):
+    """
+    Create ranges to request rows data to google sheets api.
+    """
+
+    parameters: Mapping[str, Any]
+
+    def __init__(self, parameters: Mapping[str, Any]) -> None:
+        super().__init__(parameters)
+        self.parameters = parameters
+        self.sheet_row_count = parameters.get("row_count", 0)
+        self.sheet_id = parameters.get("sheet_id")
+        self.batch_size = parameters.get("batch_size")
+
+    def stream_slices(self) -> Iterable[StreamSlice]:
+        start_range = 2  # skip 1 row, as expected column (fields) names there
+
+        while start_range <= self.sheet_row_count:
+            end_range = start_range + self.batch_size
+            logger.info(f"Fetching range {self.sheet_id}!{start_range}:{end_range}")
+            yield StreamSlice(partition={"start_range": start_range, "end_range": end_range}, cursor_slice={})
+            start_range = end_range + 1
+
+
+# Spreadsheet Models
+
+
+class ValueRange(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    values: Optional[List[List[str]]] = None
+
+
+class SpreadsheetValues(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    spreadsheetId: str
+    valueRanges: List[ValueRange]
+
+
+class SpreadsheetProperties(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    title: Optional[str] = None
+
+
+class SheetProperties(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    title: Optional[str] = None
+
+
+class CellData(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    formattedValue: Optional[str] = None
+
+
+class RowData(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    values: Optional[List[CellData]] = None
+
+
+class GridData(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    rowData: Optional[List[RowData]] = None
+
+
+class Sheet(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    data: Optional[List[GridData]] = None
+    properties: Optional[SheetProperties] = None
+
+
+class Spreadsheet(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+    spreadsheetId: str
+    sheets: List[Sheet]
+    properties: Optional[SpreadsheetProperties] = None
+
+
+# UTILS code
+
+
+TOKEN_PATTERN = re.compile(r"[A-Z]+[a-z]*|[a-z]+|\d+|(?P<NoToken>[^a-zA-Z\d]+)")
+DEFAULT_SEPARATOR = "_"
+
+
+def name_conversion(text: str) -> str:
+    """
+    convert name using a set of rules, for example: '1MyName' -> '_1_my_name'
+    """
+    # Normalize and remove accents
+    text = unicodedata.normalize("NFD", text)
+    text = "".join(char for char in text if unicodedata.category(char) != "Mn")
+    # Then remove remaining non-ASCII
+    text = text.encode("ascii", errors="ignore").decode("ascii")
+
+    tokens = []
+    for m in TOKEN_PATTERN.finditer(text):
+        if m.group("NoToken") is None:
+            tokens.append(m.group(0))
+        else:
+            tokens.append("")
+
+    if len(tokens) >= 3:
+        tokens = tokens[:1] + [t for t in tokens[1:-1] if t] + tokens[-1:]
+
+    if tokens and tokens[0].isdigit():
+        tokens.insert(0, "")
+
+    text = DEFAULT_SEPARATOR.join(tokens)
+    text = text.lower()
+    return text
+
+
+def safe_name_conversion(text: str) -> str:
+    if not text:
+        return text
+    new = name_conversion(text)
+    if not new:
+        raise Exception(f"initial string '{text}' converted to empty")
+    return new
+
+
+def exception_description_by_status_code(code: int, spreadsheet_id) -> str:
+    if code in [status_codes.INTERNAL_SERVER_ERROR, status_codes.BAD_GATEWAY, status_codes.SERVICE_UNAVAILABLE]:
+        return (
+            "There was an issue with the Google Sheets API. This is usually a temporary issue from Google's side."
+            " Please try again. If this issue persists, contact support"
+        )
+    if code == status_codes.FORBIDDEN:
+        return (
+            f"The authenticated Google Sheets user does not have permissions to view the spreadsheet with id {spreadsheet_id}. "
+            "Please ensure the authenticated user has access to the Spreadsheet and reauthenticate. If the issue persists, contact support"
+        )
+    if code == status_codes.NOT_FOUND:
+        return (
+            f"The requested Google Sheets spreadsheet with id {spreadsheet_id} does not exist. "
+            f"Please ensure the Spreadsheet Link you have set is valid and the spreadsheet exists. If the issue persists, contact support"
+        )
+
+    if code == status_codes.TOO_MANY_REQUESTS:
+        return "Rate limit has been reached. Please try later or request a higher quota for your account."
+
+    return ""
